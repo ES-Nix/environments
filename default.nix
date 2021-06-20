637fe5cbcabf2c6febbf781fa29b5d587a7dc64a
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-19 21:52:05 -0300

    Test

--- default.nix
+++ default.nix
@@ -1,15 +1,16 @@
 { pkgs ?  import <nixpkgs> {} }:
 pkgs.stdenv.mkDerivation {
   name = "minimal";
-  buildInputs = with pkgs; [ bashInteractive coreutils ];
+  buildInputs = with pkgs; [ bashInteractive coreutils hello ];
 
   # https://nix.dev/anti-patterns/language#reproducability-referencing-top-level-directory-with
   src = builtins.path { path = ./.; name = "minimal"; };
   installPhase = ''
     mkdir --parent $out/bin
 
-    install -t $out/bin ${pkgs.bashInteractive}/bin
-    install -t $out/bin ${pkgs.coreutils}/bin
+    install -t $out/bin ${pkgs.bashInteractive}/bin/bash
+    install -t $out/bin ${pkgs.coreutils}/bin/coreutils
+    install -t $out/bin ${pkgs.hello}/bin/hello
   '';
 
   phases = [ "buildPhase" "installPhase" "fixupPhase" ];
