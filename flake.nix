3e2e39cb4eb35eaef8a65a22f83c1465988eb643
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-20 02:03:34 -0300

    Adds bashInteractive

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           self.packages.${system}.dev
-          lesspipe
+          bashInteractive
         ];
 
         shellHook = ''
