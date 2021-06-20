215bfb832ecdec3230e225fbb29bfcdfb560f918
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-19 21:23:48 -0300

    Another try

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ pkgs.stdenv.mkDerivation {
   src = builtins.path { path = ./.; name = "minimal"; };
   installPhase = ''
     mkdir --parent $out/bin;
-    install -t $out/bin ${pkgs.bashInteractive}/bin/bash
-    install -t $out/bin ${pkgs.coreutils}/bin/coreutils
+    install -t $out/bin ${pkgs.bashInteractive}/bin
+    install -t $out/bin ${pkgs.coreutils}/bin
   '';
 
   phases = [ "buildPhase" "installPhase" "fixupPhase" ];
